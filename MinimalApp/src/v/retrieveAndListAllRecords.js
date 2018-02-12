da38743e60f89c38410ba08e7acfc9c177da7612
Mrebb <madhu.rebbana@gmail.com>
2018-02-11 21:05:29 -0800

    StudentRecordUpdate

--- MinimalApp/src/v/retrieveAndListAllRecords.js
+++ MinimalApp/src/v/retrieveAndListAllRecords.js
@@ -13,9 +13,9 @@
     for (i=0; i < keys.length; i++) {
       key = keys[i];
       row = tableBodyEl.insertRow();
-      row.insertCell(-1).textContent = Record.instances[key].isbn;      
-      row.insertCell(-1).textContent = Record.instances[key].title;  
-      row.insertCell(-1).textContent = Record.instances[key].year;
+      row.insertCell(-1).textContent = Record.instances[key].studentName;      
+      row.insertCell(-1).textContent = Record.instances[key].grade;  
+      row.insertCell(-1).textContent = Record.instances[key].age;
     }
   }
 };
